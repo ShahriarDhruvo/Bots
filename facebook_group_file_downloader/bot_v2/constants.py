4b363a9749dd0093ddc11a64a700fc8b419d0372
ShahriarDhruvo <shahriarelahi3062@gmail.com>
2022-03-24 21:29:20 +0600

    v2 finalized

--- facebook_group_file_downloader/bot_v2/constants.py
+++ facebook_group_file_downloader/bot_v2/constants.py
@@ -1,21 +1,26 @@
+# Change these values according to your needs
 TIMEOUT = 1800  # 30 minutes
 ENCODING = "utf-8"
 EXPLICIT_WAIT_TIME = 10
 TARGET_FILE_TYPE = "pdf"
+LOG_FILE_NAME = "bot.log"
 NORMALIZATION_FORM = "NFC"
-LOG_FILE_LOCATION = "bot.log"
-DOWNLOAD_DIRECTORY = "/mnt/762C61C32C617F4D/Download/Bangla-Books-Direct-Link"
-FACEBOOK_GROUP_URL = "https://www.facebook.com/groups/blbookdl/files/"
+TRACKER_FILE_NAME = "registered_files2.json"
+DOWNLOAD_DIRECTORY = "D:\\Download\\Bangla-Books-Direct-Link\\"
+FACEBOOK_GROUP_URL = "https://www.facebook.com/groups/blbookdl"
 # FACEBOOK_GROUP_URL = "https://www.facebook.com/groups/201623576939858/files/"
+
 
 # Turn it off if you find it is causing duplicate files
 SHOULD_CHECK_LOCAL_FILES = False
 
 # This file keeps track of every downloaded file
-TRACKER_FILE_LOCATION = "registered_files2.json"
+TRACKER_FILE_LOCATION = "./" + TRACKER_FILE_NAME
+LOG_FILE_LOCATION = "./" + LOG_FILE_NAME
 
 # For chromium based browsers this is '.crdownload'
 PARTIALLY_DOWNLOADED_FILE_EXT = ".crdownload"
+FILE_URL = FACEBOOK_GROUP_URL + "/files/"
 
 
 """
@@ -33,11 +38,9 @@ WARNING
 name_xpath, type_date_xpath -> this identifiers will change continuously so update it according to your needs 
 
 """
-OPTION_BUTTON_XPATH = "//div[@aria-label='File options']"
+OPTION_BUTTON_XPATH = "//div[@aria-label='File Options']"
 DOWNLOAD_BUTTON_XPATH = "//a[contains(@href, 'https://www.facebook.com/download/')]"
-POST_PERMALINK_XPATH = (
-    "//a[contains(@href, 'https://www.facebook.com/groups/201623576939858/permalink/')]"
-)
+POST_PERMALINK_XPATH = "//a[contains(@href, '" + FACEBOOK_GROUP_URL + "/permalink/')]"
 
 NAME_XPATH = "//span[@class='d2edcug0 hpfvmrgz qv66sw1b c1et5uql lr9zc1uh a8c37x1j fe6kdd0r mau55g9w c8b282yb keod5gw0 nxhoafnm aigsh9s9 d3f4x2em iv3no6db jq4qci2q a3bd9o3v lrazzd5p oo9gr5id hzawbc8m']"
 TYPE_DATE_XPATH = "//span[@class='d2edcug0 hpfvmrgz qv66sw1b c1et5uql lr9zc1uh a8c37x1j fe6kdd0r mau55g9w c8b282yb keod5gw0 nxhoafnm aigsh9s9 d9wwppkn iv3no6db e9vueds3 j5wam9gi b1v8xokw oo9gr5id hzawbc8m']"
